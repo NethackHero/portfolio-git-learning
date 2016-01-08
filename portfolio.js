d3564d02d16a9d05c805898549da69381151e445
Tony Tan <tonyyjt@gmail.com>
2016-01-08 20:42:14 +1100

    Here is the actual smooth scrolling modification, previous commit was from yesterday

---
+++ portfolio.js
@@ -0,0 +1,20 @@
+
+/**
+$('.nav>li>a').click(function(){
+		$('html, body').animate( 
+		{ scrollTop: $( $(this).attr('href') ).offset().top } 
+		, 500 );
+	});
+**/
+
+
+$(document).ready(function(){
+	
+	$('.nav>li>a').click(function(){
+			$('html, body').animate( 
+			{ scrollTop: $( $(this).attr('href') ).offset().top } 
+			, 500 );
+		});
+	});
+
+
