5a720bdbc040f567a969d39ea462e9a8c9d957a9
Tony Tan <tonyyjt@gmail.com>
2016-01-11 20:48:10 +1100

    transition of nav background color

--- portfolio.js
+++ portfolio.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 	$('.nav>li>a').click(function(){
 			$('html, body').animate( 
 			{ scrollTop: $( $(this).attr('href') ).offset().top } 
-			, 2000 );
+			, 1000 );
 		});
 	});
 
