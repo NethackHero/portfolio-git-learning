79520b6f6ca819a2e5f28a85feb8fa668570310e
Tony Tan <tonyyjt@gmail.com>
2016-01-11 17:48:28 +1100

    Picture improves, nav bar, took some code off

--- portfolio.js
+++ portfolio.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 	$('.nav>li>a').click(function(){
 			$('html, body').animate( 
 			{ scrollTop: $( $(this).attr('href') ).offset().top } 
-			, 500 );
+			, 2000 );
 		});
 	});
 
